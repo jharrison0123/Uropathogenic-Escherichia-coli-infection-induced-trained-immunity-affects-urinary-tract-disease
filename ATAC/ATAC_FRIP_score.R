f15f0735225e9ab10dd16156c7a556fb2a2a3d05
jharrison0123 <62157151+jharrison0123@users.noreply.github.com>
2023-02-03 18:15:17 -0600

    Update ATAC_FRIP_score.R

--- ATAC/ATAC_FRIP_score.R
+++ ATAC/ATAC_FRIP_score.R
@@ -1,1 +1,16 @@
+#R 3.6.1
+## Step 1. DiffBind
+## https://bioconductor.org/packages/3.10/bioc/vignettes/DiffBind/inst/doc/DiffBind.pdf
+## https://bioinformatics-core-shared-training.github.io/cruk-summer-school-2020/ChIPSeq/scripts/ChIP_Practical3_DiffBind.html
+
+setwd("1_diffbind")
+
+##CUT&RUN 
+## 1.1. read in data
+samples = read.csv("sample.csv")
+#usinng deduplicated bam and narrowpeak ATAC
+samples_dba = dba(sampleSheet=samples)
+
+## 1.2. get count matrix
+samples_count = dba.count(samples_dba) 
 
