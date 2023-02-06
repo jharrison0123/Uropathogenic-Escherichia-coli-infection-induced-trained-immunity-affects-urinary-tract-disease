d34a925e69cc2d035934cfb0c9c456d24b8fa04b
jharrison0123 <62157151+jharrison0123@users.noreply.github.com>
2023-02-06 12:50:02 -0600

    Update WGBS_R_bimodal.r

--- WGBS/WGBS_R_bimodal.r
+++ WGBS/WGBS_R_bimodal.r
@@ -1,35 +1,36 @@
-naive1_cov.df <- as.data.frame(read.table("/scratch/jharrison/Seongmi/WGBS/2_bismark/cov_rename/N1_deduplicated.bismark.cov_coverage_5x.txt", header=F))
 #R version 3.6.1 
 
 library(ggplot2)
 
-naive3_cov.df <- as.data.frame(read.table("N3_deduplicated.bismark.cov_coverage_5x.txt", header=F))
-resolved1_cov.df <- as.data.frame(read.table("R1_deduplicated.bismark.cov_coverage_5x.txt", header=F))
-resolved4_cov.df <- as.data.frame(read.table("R4_deduplicated.bismark.cov_coverage_5x.txt", header=F))
-sensitized1_cov.df <- as.data.frame(read.table("S1_deduplicated.bismark.cov_coverage_5x.txt", header=F))
-sensitized2_cov.df <- as.data.frame(read.table("S2_deduplicated.bismark.cov_coverage_5x.txt", header=F))
+##using methylc track
+mn1 <- data.frame(read.table(file="/WGBS/6_track_mergedCG/rename/N1._methylc_5x.txt", header=F))
+mn3 <- data.frame(read.table(file="/WGBS/6_track_mergedCG/rename/N3._methylc_5x.txt", header=F))
+mr1 <- data.frame(read.table(file="/WGBS/6_track_mergedCG/rename/R1._methylc_5x.txt", header=F))
+mr4 <- data.frame(read.table(file="/WGBS/6_track_mergedCG/rename/R4._methylc_5x.txt", header=F))
+ms1 <- data.frame(read.table(file="/WGBS/6_track_mergedCG/rename/S1._methylc_5x.txt", header=F))
+ms2 <- data.frame(read.table(file="/WGBS/6_track_mergedCG/rename/S2._methylc_5x.txt", header=F))
 
 
+bimodal_methylc <-ggplot(data=mn1, aes(V5)) + 
+    geom_density( colour='#2832f7', fill='#2832f7',   alpha=0.05,  adjust=3) +
+  geom_density(data = mn3, alpha=0.05, colour='#0812cf', fill='#0812cf',  alpha=0.05,  adjust=3) + 
+  geom_density(data = mr1, alpha=0.05, colour='#facd07', fill='#facd07',  alpha=0.05,  adjust=3) +
+    geom_density(data = mr4, alpha=0.05, colour='#f5da64', fill='#f5da64',  alpha=0.05,  adjust=3) +
+    geom_density(data = ms1, alpha=0.05, colour='#cc6cf5', fill='#cc6cf5',  alpha=0.05,  adjust=3) +
+    geom_density(data = ms2, alpha=0.05, colour='#ae02f7', fill='#ae02f7',  alpha=0.05,  adjust=3) + 
+  xlab("CpG Methylation") +
+  ylab("Density") + 
+  scale_fill_manual(name='', breaks=c('N1', 'N3', 'R1', 'R4','S1' , 'S2'), values=c('N1' = '#2832f7', 'N3' = '#0812cf', 'R1' = '#facd07', 'R4'= '#f5da64','S1' = '#cc6cf5', 'S2' = '#ae02f7')) +
+   scale_color_manual(name='', breaks=c('N1', 'N3', 'R1', 'R4','S1' , 'S2'), values=c('N1' = '#2832f7', 'N3' = '#0812cf', 'R1' = '#facd07', 'R4'= '#f5da64','S1' = '#cc6cf5', 'S2' = '#ae02f7')) +
+  theme_bw() +
+  scale_x_continuous("CpG Methylation",breaks=c(0, .25, .50, .75 , 1.00), limits=c(0, 1), expand = c(0.01, 0.01)) +     
+  theme(text = element_text(size=30)) 
 
-naive1_cov.plot <- ggplot(naive1_cov.df, aes(V3)) + geom_histogram( binwidth = 7, alpha=0.2, colour="darkblue", fill="darkblue") 
-naive3_cov.plot <- ggplot(naive3_cov.df, aes(V3)) + geom_histogram( binwidth = 7, alpha=0.2, colour="darkgreen", fill="darkgreen") 
-resolved1_cov.plot <- ggplot(resolved1_cov.df, aes(V3)) + geom_histogram( binwidth = 7, alpha=0.2, colour="darkred", fill="darkred") 
-resolved4_cov.plot <- ggplot(resolved4_cov.df, aes(V3)) + geom_histogram( binwidth = 7, alpha=0.2, colour="yellow", fill="yellow") 
-sensitized1_cov.plot <- ggplot(sensitized1_cov.df, aes(V3)) + geom_histogram( binwidth = 7, alpha=0.2, colour="darkorange", fill="darkorange") 
-sensitized2_cov.plot <- ggplot(sensitized2_cov.df, aes(V3)) + geom_histogram( binwidth = 7, alpha=0.2, colour="purple", fill="purple") 
-
-colors <- c('N1'="darkblue", 'N3'="darkgreen", 'R1'="darkred", 'R4'="darkyellow", 'S1'="darkorange", 'S2'="darkpurple")
-
-bimodal <-ggplot(data=naive1_cov.df, aes(V3)) + 
-  geom_histogram( alpha=0.2, colour="darkblue", fill="darkblue", binwidth = 7) + 
-  geom_histogram(data = naive3_cov.df, alpha=0.2, colour="darkgreen", fill="darkgreen", binwidth = 7) + 
-  geom_histogram(data = resolved1_cov.df, alpha=0.2, colour="darkred", fill="darkred", binwidth = 7) +
-    geom_histogram(data = resolved4_cov.df, alpha=0.2, colour="yellow", fill="yellow", binwidth = 7) +
-    geom_histogram(data = sensitized1_cov.df, alpha=0.2, colour="darkorange", fill="darkorange", binwidth = 7) +
-    geom_histogram(data = sensitized2_cov.df, alpha=0.2, colour="purple", fill="purple", binwidth = 7) + labs(x="CpG Methylation (%)", y="Density", colors = "Legend") + legend('topright', c('N1', 'N3', 'R1', 'R4', 'S1', 'S2'), fill=c('blue', 'green', 'red', 'yellow', 'orange', 'purple'))
    
-bimodal
+    #guides(fill = guide_legend(override.aes = list(color="NA", size=10, stroke=0.01) ) ) +
 
-pdf("bimodal.pdf")
-bimodal
+bimodal_methylc
+
+pdf("bimodal_methylc_adjust3.pdf")
+bimodal_methylc
 dev.off()
